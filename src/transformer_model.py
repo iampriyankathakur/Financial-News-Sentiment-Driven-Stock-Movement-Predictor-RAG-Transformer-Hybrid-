dfdb72097a4694efa373a6a5f28215d2261c2637
Priyanka Thakur <55808489+iampriyankathakur@users.noreply.github.com>
2026-02-26 09:37:57 -0500

    Update transformer_model.py

--- src/transformer_model.py
+++ src/transformer_model.py
@@ -1,12 +1,24 @@
 import torch.nn as nn
 
 class StockTransformer(nn.Module):
+
     def __init__(self, input_dim):
         super().__init__()
-        self.fc = nn.Sequential(
-            nn.Linear(input_dim, 64),
-            nn.ReLU(),
-            nn.Linear(64,2)
+        self.attention = nn.MultiheadAttention(
+            embed_dim=input_dim,
+            num_heads=1,
+            batch_first=True
         )
+        self.fc = nn.Linear(input_dim,2)
+
     def forward(self,x):
-        return self.fc(x)
+        x = x.unsqueeze(1)
+        attn_output,_ = self.attention(x,x,x)
+        return self.fc(attn_output.squeeze(1))
+
+
+attn_output, attn_weights = model.attention(x,x,x)
+
+import matplotlib.pyplot as plt
+plt.imshow(attn_weights.detach().numpy())
+plt.colorbar()
